1a713966cd054c7a61dcff772d74ace0d90d9281
wanghong <wh919551518@163.com>
2019-12-26 13:29:29 +0800

    wh

---
+++ day01/src/main/java/club/banyuan/day01/Day01Application.java
@@ -0,0 +1,11 @@
+package club.banyuan.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Day01Application.class,args);
+    }
+}
---
+++ day01/src/main/java/club/banyuan/day01/controller/PersonController.java
@@ -0,0 +1,73 @@
+package club.banyuan.day01.controller;
+
+import club.banyuan.day01.dao.PersonDao;
+import club.banyuan.day01.dao.PersonDao;
+import club.banyuan.day01.model.Person;
+import club.banyuan.day01.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+
+@RestController
+public class PersonController {
+    @Autowired
+    PersonDao personDao;
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/personAll")
+    public List<Person> findAll(HttpServletResponse resp) throws IOException {
+
+        resp.setContentType("text/html;charset=utf-8");
+        List<Person> person = personDao.findAll();
+        for (Person p : person) {
+            resp.getWriter().write(p.toString() + "<br/>");
+        }
+        return null;
+    }
+
+    @GetMapping("/person")
+    public List<Person> findAllById(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        String id = req.getParameter("id");
+        List<Person> person = personDao.findAllById(Collections.singleton(Integer.parseInt(id)));
+        for (Person p : person) {
+            resp.getWriter().write(p.toString() + "=====>");
+        }
+        return null;
+    }
+
+    @PostMapping("/person")
+    public void addPerson(HttpServletRequest req, HttpServletResponse resp) throws IOException, InvocationTargetException, IllegalAccessException, ParseException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String gender = req.getParameter("gender");
+        String birthday = req.getParameter("birthday");
+        String address = req.getParameter("address");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        Date parse = dateFormat.parse(birthday);
+
+
+        personDao.save(new Person(name, gender, parse, address));
+
+    }
+
+}
+
