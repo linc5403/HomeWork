68c74f5a87136d32b5ace998a5cdaf7546ca3689
linchuan <linc1982@icloud.com>
2019-12-30 00:06:36 +0800

    change mapper to repository

--- practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/dao/UserDao.java
+++ practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/dao/UserDao.java
@@ -4,10 +4,11 @@ import club.banyuan.springbootmybatis.bean.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 public interface UserDao {
     @Select("SELECT * FROM user WHERE name = #{name}")
     List<User> findUserByName(@Param("name") String name);
