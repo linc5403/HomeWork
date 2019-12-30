cf02912208814ccc9d386db1413702eb0ef3c6c5
linchuan <linc1982@icloud.com>
2019-12-30 09:00:58 +0800

    mybatis-xml

--- practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/dao/UserDao.java
+++ practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/dao/UserDao.java
@@ -2,7 +2,6 @@ package club.banyuan.springbootmybatis.dao;
 
 import club.banyuan.springbootmybatis.bean.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +9,5 @@ import java.util.List;
 
 @Repository
 public interface UserDao {
-    @Select("SELECT * FROM user WHERE name = #{name}")
-    List<User> findUserByName(@Param("name") String name);
-
-    @Select("SELECT * FROM user")
-    List<User> findAll();
+    List<User> findAllUser();
 }
--- practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/service/UserService.java
+++ practice/springboot-mybatis/src/main/java/club/banyuan/springbootmybatis/service/UserService.java
@@ -1,9 +1,9 @@
 package club.banyuan.springbootmybatis.service;
 
 import club.banyuan.springbootmybatis.bean.User;
-import club.banyuan.springbootmybatis.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import club.banyuan.springbootmybatis.dao.UserDao;
 
 import java.util.List;
 
@@ -13,6 +13,6 @@ public class UserService {
     private UserDao userDao;
 
     public List<User> selectAllUser() {
-        return userDao.findAll();
+        return userDao.findAllUser();
     }
 }
